14410be9b8b9e78067ac3c704cf0cb1ee0b05f56
Ivan Znak <ivanznak04@gmail.com>
2023-12-04 13:44:02 +0100

    Code was changed

--- HomeworkPanGestureRecognizer/HomeworkPanGestureRecognizer/ViewController.swift
+++ HomeworkPanGestureRecognizer/HomeworkPanGestureRecognizer/ViewController.swift
@@ -18,7 +18,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        staticView = UIView(frame: CGRect(x: 50, y: 100, width: 100, height: 100))
+        staticView = UIView(frame: CGRect(x: view.center.x , y: view.frame.maxY - 200, width: 100, height: 100))
+        
         staticView.backgroundColor = UIColor.blue
         view.addSubview(staticView)
 
@@ -31,11 +32,11 @@ class ViewController: UIViewController {
         
         resetButton = UIButton(type: .system)
         resetButton.setTitle("", for: .normal)
-        resetButton.frame = CGRect(x: 50, y: 300, width: 200, height: 40)
+        resetButton.frame = CGRect(x: view.center.x/2, y: view.center.y, width: 200, height: 40)
         resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
         view.addSubview(resetButton)
     }
-
+    
     @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: view)
         if pointCheckStatus == 0 {
@@ -55,7 +56,7 @@ class ViewController: UIViewController {
     }
 
     @objc func resetButtonTapped() {
-        staticView.frame = CGRect(x: 50, y: 100, width: 100, height: 100)
+        staticView.frame = CGRect(x: view.center.x , y: view.frame.maxY - 200, width: 100, height: 100)
         movingView.frame = CGRect(x: 200, y: 100, width: 100, height: 100)
         pointCheckStatus = 0
         resetButton.setTitle("", for: .normal)
